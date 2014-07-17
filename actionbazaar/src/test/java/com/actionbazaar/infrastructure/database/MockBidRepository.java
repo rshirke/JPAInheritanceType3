b9e73f5774f2e57a142bf476a8310c7b0e62e3e9
Reza Rahman <reza_rahman@lycos.com>
2014-07-17 16:57:33 -0400

    Cleanup

--- actionbazaar/src/test/java/com/actionbazaar/infrastructure/database/MockBidRepository.java
+++ actionbazaar/src/test/java/com/actionbazaar/infrastructure/database/MockBidRepository.java
@@ -37,79 +37,36 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.actionbazaar.application;
+package com.actionbazaar.infrastructure.database;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import javax.ejb.EJB;
-
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.actionbazaar.domain.Bid;
 import com.actionbazaar.domain.BidRepository;
-import com.actionbazaar.infrastructure.database.MockBidRepository;
+import com.actionbazaar.domain.Bid;
 
-@RunWith(Arquillian.class)
-public class BidServiceUnitTest {
+public class MockBidRepository implements BidRepository {
 
-    @Deployment
-    public static WebArchive createDeployment() {
-        return ShrinkWrap
-                .create(WebArchive.class, "actionbazaar-test.war")
-                .addClasses(BidService.class, DefaultBidService.class,
-                        BidRepository.class, MockBidRepository.class, Bid.class)
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    @Override
+    public Bid addBid(Bid bid) {
+        return bid;
     }
 
-    @EJB
-    private BidService bidService;
-
-    @Test
-    public void testAddBid() {
-        // Save a new bid.
+    @Override
+    public Bid getBid(Long id) {
         Bid bid = new Bid();
-        bid.setBidder("rrahman");
+        bid.setId(id);
+        bid.setBidder("nrahman");
         bid.setItem("Test item");
-        bid.setAmount(100.50);
+        bid.setAmount(100.00);
 
-        bidService.addBid(bid);
-
-        // Make sure it was "correctly" saved.
-        bid = bidService.getBid(1L);
-
-        assertEquals("nrahman", bid.getBidder());
-        assertEquals("Test item", bid.getItem());
-        assertEquals(new Double(100.00), bid.getAmount());
+        return bid;
     }
 
-    @Test
-    public void testUpdateBid() {
-        // Update bid.
-        Bid bid = bidService.getBid(1L);
-        bid.setAmount(101.50);
-        bidService.updateBid(bid);
-
-        // Make sure bid was "correctly" updated.
-        bid = bidService.getBid(1L);
-
-        assertEquals("nrahman", bid.getBidder());
-        assertEquals("Test item", bid.getItem());
-        assertEquals(new Double(100.00), bid.getAmount());
+    @Override
+    public void updateBid(Bid bid) {
+        // NOOP
     }
 
-    @Test
-    public void testDeleteBid() {
-        Bid bid = bidService.getBid(1L);
-        bidService.deleteBid(bid);
-
-        // Make sure it was "correctly" deleted.
-        assertNotNull(bidService.getBid(1L));
+    @Override
+    public void deleteBid(Bid bid) {
+        // NOOP
     }
 }
